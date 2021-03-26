85f5a8732b461c4f8d448eab58de9c861c540f1b
doodlesun <76955830+doodlesun@users.noreply.github.com>
2021-03-26 22:42:32 +0100

    add all tickers to price list, no price discovered is marked as such

--- filtering.py
+++ filtering.py
@@ -2,10 +2,12 @@ import re
 
 import logger
 import requests
+import json
 from datetime import datetime 
 
 coingeckoUrl = 'https://api.coingecko.com/api/v3'
 allCoins = requests.get(coingeckoUrl + '/coins/list')
+logger.printError(allCoins)
 
 def filterForTokens(data):
     regex = re.compile(r'(?!.*[BTC]|[ETH])([\$|\#][A-Z0-1]{2,})')
@@ -17,6 +19,16 @@ def filterForTokens(data):
             if match := regex.findall(entry['retweet']):
                 extractTicker(entry, match, filtered)
     return filtered
+
+# Might be useful later on
+def checkForToken(entry, filtered):
+    foundTicker = []
+    for ticker in allCoins.json():
+        if ticker['name'] in entry['text']:
+            print(entry['text'])
+            filtered.append(entry)
+            print('foundTicker', ticker)
+    return foundTicker
 
 def extractTicker(entry, match, filtered):
         replacedMatches = []
@@ -30,22 +42,25 @@ def getTokenPrice(tokenArr):
     lowerTokenArr = []
     for entryToken in tokenArr:
         lowerTokenArr.append(entryToken.lower())
-    logger.printError(allCoins)
     ids = []
     tokens = {}
     for coin in allCoins.json():
         if coin['symbol'] in lowerTokenArr:
             ids.append(coin['id'])
-            tokens[coin['id']] = coin['symbol']
+            tokens[f'${coin["symbol"].upper()}'] = coin['id']
     returnValue = {}
     if len(ids) > 0:
         params = {'ids': ','.join(ids) , 'vs_currencies': 'usd' }
         price = requests.get(coingeckoUrl + '/simple/price', params=params)
         logger.printError(price)
         response = price.json()
-        for tokenId in list(dict.fromkeys(tokens)):
-            response[tokenId]['symbol'] = tokens[tokenId].upper()
-            response[tokenId]['time'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-        returnValue = response
+        for symbol, tokenId in tokens.items():
+            returnValue[symbol] = {}
+            returnValue[symbol]['usd'] = response[tokenId]['usd']
+            returnValue[symbol]['time'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+    for coin in lowerTokenArr:
+        upperCoin = f'${coin.upper()}' 
+        if upperCoin not in list(dict.fromkeys(returnValue)):
+            returnValue[upperCoin] = {'usd': 'no_price'}
     return returnValue;
 
